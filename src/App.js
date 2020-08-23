0821356ceda539213d2789cbf09e827164f3b73e
Danielle Arias <daniellearias321@gmail.com>
2020-08-23 12:03:43 -0700

    added sort function for age

--- src/App.js
+++ src/App.js
@@ -1,21 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "./utils/Api";
 import "./App.css";
-import Form from "./components/Form";
-import DataBody from "./components/DataBody";
-import DataTable from "./components/DataTable";
 
-function App() {
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [allUsers, setAllUsers] = useState([]);
+
+  useEffect(() => {
+    API.getUsers().then(({ data }) => {
+      setUsers(data.results);
+      setAllUsers(data.results);
+    });
+  }, []);
+
+  const search = (event) => {
+    const searchValue = event.target.value;
+
+    const newUsers = allUsers.filter((user) => {
+      if (user.name.first.toLowerCase().includes(searchValue.toLowerCase())) {
+        return true;
+      }
+      return false;
+    });
+    setUsers(newUsers);
+  };
+
+  const sort = () => {
+    const sortedUsers = users.sort((a, b) => {
+      if (a.dob.age < b.dob.age) {
+        return -1;
+      }
+      if (a.dob.age > b.dob.age) {
+        return 1;
+      }
+      return 0;
+    });
+    setUsers([...sortedUsers]);
+  };
+
+  console.log(users);
+
   return (
-    <div className="App">
-      <div className="container">
-        <div className="jumbotron text-center rounded-0">
-          <h1>Employee Directory</h1>
-          <Form></Form>
-        </div>
-        <DataTable />
-      </div>
+    <div>
+      <input onChange={search} />
+      <button onClick={sort}> Sort by age</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id.value}>
+              <th>{user.name.first}</th>
+              <th>{user.name.last}</th>
+              <th>{user.email}</th>
+              <th>{user.phone}</th>
+              <th>{user.dob.age}</th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
-
-export default App;
